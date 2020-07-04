0a65f512e2224d3bc4d16529916cdb4bf3c60861
aks <89146748462@ya.ru>
2020-07-04 17:51:00 +1000

    userInfoPage

--- src/main/java/ru/project/controller/UserController.java
+++ src/main/java/ru/project/controller/UserController.java
@@ -1,24 +1,40 @@
 package ru.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.project.service.UserService;
+import ru.project.model.Role;
+import ru.project.model.User;
+import ru.project.service.UserUserServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private UserUserServiceImpl userUserService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserUserServiceImpl userUserService) {
+        this.userUserService = userUserService;
+    }
 
     @GetMapping("/me")
-    public String hello(ModelMap modelMap, HttpServletRequest httpSession){
-        modelMap.addAttribute("user", httpSession.getSession().getAttribute("user"));
+    public String hello(ModelMap modelMap, HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();//
+        User user = (User) authentication.getPrincipal();
+        Set<Role> set = user.getRoles();
+        String text = set.toString().replaceAll("(?u)[^\\pL ]", "");
+        modelMap.addAttribute("email", user.getEmail());
+        modelMap.addAttribute("roles", text);
+        modelMap.addAttribute("user",user);
         return "user";
     }
 }
